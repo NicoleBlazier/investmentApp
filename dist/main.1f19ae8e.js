5078d40772d353cc7ea09f446ef05a25c42a2f8a
Nicole Blazier <nicole.blazier@gmail.com>
2023-04-21 20:44:24 -0500

    Add Mobile Screen to First Stepper

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -696,7 +696,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58059" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49195" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
